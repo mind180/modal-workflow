79333620910206c58c12e601dcf212aacbdb8edf
billjoynf <45283458+billjoynf@users.noreply.github.com>
2020-09-10 19:03:14 +0300

    refactor: remove import

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 import {WorkflowModal} from "/WorkflowModal.js";
 import {LoginStep} from "/Steps/LoginStep.js";
-import {UserInfoStep} from "/Steps/UserInfoStep.js";
 
 const workflowModal = new WorkflowModal(new LoginStep());
 document.body.appendChild(workflowModal);
