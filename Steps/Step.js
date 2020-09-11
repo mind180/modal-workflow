6122e6251a544766e1d9ffe15c35f6554b814b05
billjoynf <45283458+billjoynf@users.noreply.github.com>
2020-09-11 18:57:12 +0300

    feat: add base step

---
+++ Steps/Step.js
@@ -0,0 +1,48 @@
+export class Step extends HTMLElement {
+  constructor(_modal) {
+    super();
+    this.modal = _modal;
+    this.shadow = this.attachShadow({mode: 'open'});
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadow.innerHTML = this.layout();
+  }
+
+  layout() {
+    const style = `
+        <style>
+          #fields {
+            display: flex;
+            flex-direction: column;
+          }
+        </style>
+      `;
+  
+    const template = `
+        <div id='content'>
+          <div>Login step:</div>
+          <div id='fields'>
+            <input id='login' type='text' placeholder='login'>
+            <input id='password' type='password' placeholder='password'>
+          <div>
+        </div>
+      `;
+
+    return style + template;
+  }
+
+  next() {
+    console.log("Method has not been implemented");
+  }
+
+  back() {
+    console.log("Method has not been implemented");
+  }
+}
+
+customElements.define('empty-step', Step);
