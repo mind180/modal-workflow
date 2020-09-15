e6784d793f36f00283b0ea1668aa5740095f499a
billjoynf <45283458+billjoynf@users.noreply.github.com>
2020-09-15 23:18:08 +0300

    feat: modal launcher

--- client.js
+++ client.js
@@ -2,6 +2,16 @@ import {WorkflowModal} from "/WorkflowModal.js";
 import {LoginStep} from "/Steps/UserInfoFlow/LoginStep.js";
 import {LoadingBars} from "/Effects/LoadingBars.js";
 
-const initialLoginStep = new LoginStep();
-const workflowModal = new WorkflowModal(initialLoginStep, new LoadingBars());
-document.body.appendChild(workflowModal);
+function initHandlers() {
+  const workflowButton = document.getElementById('workflowButton');
+  workflowButton.addEventListener('click', openWorkflowHandler);
+}
+
+
+function openWorkflowHandler(event) {
+  const initialLoginStep = new LoginStep();
+  const workflowModal = new WorkflowModal(initialLoginStep, new LoadingBars());
+  document.body.appendChild(workflowModal);
+}
+
+initHandlers();
